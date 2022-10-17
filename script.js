24d1929294563f1bc403c2a129a2121d410744d9
Srdjan Onisko <srdjan.onisko@gmail.com>
2022-10-17 14:23:24 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,108 @@
+// Player and Computer selection function
+
+const playerSelection = function () {
+  let answer = prompt("Rock, Paper, Scissors?")?.toLowerCase();
+
+  if (answer == "rock" || answer == "paper" || answer == "scissors") {
+    return answer;
+  } else {
+    alert("Please choose between Rock, Paper or Scissors");
+    playerSelection();
+  }
+};
+
+const computerSelection = function () {
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
+
+  let computerChoice;
+
+  switch (randomNumber) {
+    case 1:
+      computerChoice = "rock";
+      break;
+    case 2:
+      computerChoice = "paper";
+      break;
+    case 3:
+      computerChoice = "scissors";
+      break;
+    default:
+      console.log("Error");
+  }
+
+  return computerChoice;
+};
+
+// Helper function to capitalize first letter of a string
+
+const capitalizeFirstLetter = function (string) {
+  return string[0].toUpperCase() + string.slice(1);
+};
+
+// Function for one round play
+
+let playerScore = 0;
+let computerScore = 0;
+
+const playRound = function (playerSelection, computerSelection) {
+  const playerSelectionCapitalized = capitalizeFirstLetter(playerSelection);
+  const computerSelectionCapitalized = capitalizeFirstLetter(computerSelection);
+
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    alert(
+      `You win! ${playerSelectionCapitalized} beats ${computerSelectionCapitalized}`
+    );
+    playerScore++;
+    return playerScore;
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    alert(
+      `You win! ${playerSelectionCapitalized} beats ${computerSelectionCapitalized}`
+    );
+    playerScore++;
+    return playerScore;
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    alert(
+      `You win! ${playerSelectionCapitalized} beats ${computerSelectionCapitalized}`
+    );
+    playerScore++;
+    return playerScore;
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    alert(
+      `You lose! ${computerSelectionCapitalized} beats ${playerSelectionCapitalized}`
+    );
+    computerScore++;
+    return computerScore;
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    alert(
+      `You lose! ${computerSelectionCapitalized} beats ${playerSelectionCapitalized}`
+    );
+    computerScore++;
+    return computerScore;
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    alert(
+      `You lose! ${computerSelectionCapitalized} beats ${playerSelectionCapitalized}`
+    );
+    computerScore++;
+    return computerScore;
+  } else {
+    return alert("It's a draw");
+  }
+};
+
+// function for multiple games with score
+
+const game = function () {
+  for (let i = 0; i < 5; i++) {
+    playRound(playerSelection(), computerSelection());
+  }
+
+  if (playerScore > computerScore) {
+    return alert(`You win! Score is ${playerScore} to ${computerScore}`);
+  } else if (computerScore > playerScore) {
+    return alert(`You lose! Score is ${computerScore} to ${playerScore}`);
+  } else {
+    return alert(`It's a draw! Score is ${playerScore} to ${computerScore}`);
+  }
+};
+
+game();
